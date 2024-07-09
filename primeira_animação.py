b070b00ae77e0fd5bf6b755d24bf3b33f7a0ceb9
Bruno Ramos Galindo <137202198+Elbruno237@users.noreply.github.com>
2024-07-09 15:21:44 -0300

    Add files via upload

--- primeira_animação.py
+++ primeira_animação.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import PillowWriter
 
+%matplotlib qt
 
 metadata = dict(title ='Movie', artist = 'Bruno')
 writer = PillowWriter(fps = 15, metadata = metadata )
-
 
 fig, ax = plt.subplots()
 #ln, = ax.plot([],[],'w-')
@@ -15,14 +15,39 @@ xlist = np.linspace(0, 500, 100)
 def func(x):
     return np.log(x)*2
 
-with writer.saving(fig, 'anime.gif',100):
+with writer.saving(fig, 'anime.gif', 100):
     for x in xlist:
-        print(x)
-        y = func(xlist)
-        
+        #print(x)
+        y = func(x)
+        ax.clear()
         ax.plot(x, y, 'ro')
         ax.set_xlim(0,500)
         ax.set_ylim(0,20)
         
+        fig.canvas.draw()  
         writer.grab_frame()
         
+with writer.saving(fig, 'anime2.gif', 100):
+    for i, x in enumerate(xlist):
+        y = func(x)
+        ax.clear()
+        ax.plot(xlist[:i+1], func(xlist[:i+1]), 'r-')  # Plota a linha até o ponto atual
+        ax.plot(x, y, 'ro')  # Plota o ponto atual
+        ax.set_xlim(0, 500)
+        ax.set_ylim(0, 20)
+        
+        fig.canvas.draw()
+        writer.grab_frame()
+
+ax.clear()
+with writer.saving(fig, 'anime3.gif', 100):
+    for x in xlist:
+        #print(x)
+        y = func(x)
+        #ax.clear()
+        ax.plot(x, y, 'ro')
+        ax.set_xlim(0,500)
+        ax.set_ylim(0,20)
+        
+        fig.canvas.draw()  
+        writer.grab_frame()
